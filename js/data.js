2865c4af584f466bf911b70345e1c3b988a545f3
Teemo-shape <762502622@qq.com>
2018-08-10 10:03:33 +0800

    延保页面

---
+++ js/data.js
@@ -0,0 +1,73 @@
+var codeRes = {
+	"data": {
+        "sn" : "1458975685002548637925",
+        "model" : "L55A908CPU",
+        "type" : "整机",
+        "createdTime":1522053838000,
+        "warranty" : "2",
+        "note" : "延长保修两个月",
+        "productCode" : "240012344473878498774",
+        "pattern" : "65*5"
+    },
+    "msg":"",
+    "success":true
+}
+var phoneRes = {
+    "data":{
+        "phone":"13407971111",
+        "total" : 3,
+        "details" : [
+            {
+                "id" : "1",
+                "sn" : "1458975685002548637925",
+                "model" : "L55A908CPU",
+                "type" : "整机",
+                "createdTime":1522053838000,
+                "warranty" : "2",
+                "note" : "延长保修两个月",
+                "productCode" : "240012344473878498774",
+                "pattern" : "65*5"
+            },
+            {
+                "id" : "2",
+                "sn" : "1458975685002548637925",
+                "model" : "L55A908CPU",
+                "type" : "整机",
+                "createdTime":1522053838000,
+                "warranty" : "2",
+                "note" : "延长保修两个月",
+                "productCode" : "240012344473878498774",
+                "pattern" : "65*5"
+            },
+            {
+                "id" : "3",
+                "sn" : "1458975685002548637925",
+                "model" : "L55A908CPU",
+                "type" : "整机",
+                "createdTime":1522053838000,
+                "warranty" : "2",
+                "note" : "延长保修两个月",
+                "productCode" : "240012344473878498774",
+                "pattern" : "65*5"
+            },
+            // {
+            //     "id" : "4",
+            //     "sn" : "1458975685002548637925",
+            //     "model" : "L55A908CPU",
+            //     "type" : "整机",
+            //     "createdTime":1522053838000,
+            //     "warranty" : "2",
+            //     "note" : "延长保修两个月",
+            //     "productCode" : "240012344473878498774",
+            //     "pattern" : "65*5"
+            // }
+        ]
+    },
+    "msg":"",
+    "success":true
+}
+var res = {
+    data:[],
+    success: true,
+    msg: '该手机号暂未绑定延保凭证'
+}
